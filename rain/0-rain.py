6234ececc59b7baa0bf9ac29cca22c104a0467de
root <y.ishimwe2@alustudent.com>
2023-06-07 13:12:24 -0700

    :)

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-""" 0_main """
+"""create an algorithm for calculating the rain"""
 
 
-rain = __import__('0-rain').rain
-
-if __name__ == "__main__":
-    walls = [0, 1, 0, 2, 0, 3, 0, 4]
-    print(rain(walls))
-    walls = [2, 0, 0, 4, 0, 0, 1, 0]
-    print(rain(walls))
+def rain(walls):
+    """calculate how much water will be retained after it rains"""
+    if not walls or len(walls) < 3:
+        return 0
+    water = 0
+    for i in range(1, len(walls) - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        water += min(left, right) - walls[i]
+    return water
